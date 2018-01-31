aba08c6660a9eeeec0c47faa503a8b5c18c4af6d
Gavin Reynolds <gavin@chef.io>
2018-01-31 10:22:12 +0000

    Add version profile attribute

--- test/smoke/default/redis_1_source_installation_test.rb
+++ test/smoke/default/redis_1_source_installation_test.rb
@@ -1,14 +1,16 @@
+version = attribute('version', default: '4.0.2', description: 'Redis version')
+
 control 'redis-1-installation' do
   title 'Redis Server: Installation from source'
   desc  ''
 
-  describe directory('/opt/redis/redis-4.0.2') do
+  describe directory("/opt/redis/redis-#{version}") do
     it { should exist }
     it { should be_owned_by 'root' }
   end
 
   %w(redis-server redis-cli).each do |bin|
-    describe file("/opt/redis/redis-4.0.2/src/#{bin}") do
+    describe file("/opt/redis/redis-#{version}/src/#{bin}") do
       it { should exist }
       it { should be_executable }
     end
